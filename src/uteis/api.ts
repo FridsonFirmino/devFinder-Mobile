9069e8ca491882d44a81c1ba5e0abc9cb3d19c49
fridsonfirmnio <fridsoncmf@gmail.com>
2023-12-31 14:46:36 +0100

    feat: Criação e implementação dos arquivos colors e api

---
+++ src/uteis/api.ts
@@ -0,0 +1,6 @@
+export const API_GitHub = "https://api.github.com/users/";
+
+export interface apiProps {
+    avatar: string
+    username: string
+}
---
+++ src/uteis/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    bg_Primary: "#141D2F",
+    bg_Secundary: "#1E2A47",
+    text_White: "#FFFFFF",
+    text_Blue: "#0079FF",
+    btn_Primary: "#0079FF", 
+}
